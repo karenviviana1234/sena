af1bcd191627f5147eee305ced71029af76ba63b
Sharit-Vargas07 <sharitvargas07@gmail.com>
2024-08-15 14:29:41 -0500

    seguimientos y personas

--- backend/src/routes/personas.route.js
+++ backend/src/routes/personas.route.js
@@ -1,12 +1,14 @@
 import Router from 'express'
-import { registrarPersona, actualizarPersona, eliminarPersona, listarPersonas } from '../controllers/personas.controller.js'
+import { registrarPersona, actualizarPersona, eliminarPersona, listarPersonas, registrarAdmin, buscarPersonas } from '../controllers/personas.controller.js'
 import { validarToken } from '../controllers/seguridad.controller.js'
 
 const rutaPersona = Router()
 
 rutaPersona.get('/listar', validarToken, listarPersonas)
+rutaPersona.get('/buscar/:id_persona', validarToken, buscarPersonas)
 rutaPersona.post('/registrar', registrarPersona)
-rutaPersona.put('/actualizar/:id', validarToken, actualizarPersona)
-rutaPersona.delete('/eliminar/:id', validarToken, eliminarPersona)
+rutaPersona.post('/registrarAdmins', registrarAdmin)
+rutaPersona.put('/actualizar/:id_persona', validarToken, actualizarPersona)
+rutaPersona.delete('/eliminar/:id_persona', validarToken, eliminarPersona)
 
 export default rutaPersona
