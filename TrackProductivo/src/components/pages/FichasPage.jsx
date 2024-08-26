5ec69de5f2d58bf404d967adcab97fb81f001ceb
Juanita252005 <juanitaguaca@gmail.com>
2024-08-26 16:45:03 -0500

    cambios en fichas

--- TrackProductivo/src/components/pages/FichasPage.jsx
+++ TrackProductivo/src/components/pages/FichasPage.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import TableFichas from '../organisms/Ficha/TableFicha'
 function FichasPage() {
   return (
     <>
-      <main className='w-full p-3'>
+
+    <>
+    <main className='w-full p-3'>
         <TableFichas/>
       </main>
     </>
-    <div>FichasPage</div>
+    <div>FichasPage</div></>
   )
 }
 
