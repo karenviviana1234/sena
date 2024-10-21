99ea0bb2a17f04ba51e5cdfc211c172dfb3123f0
Sharit-Vargas07 <sharitvargas07@gmail.com>
2024-10-21 15:59:42 -0500

    cambios finales

--- Plan_Mejoramiento/index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import App from './App';  // Asegúrate de que la ruta de `App` sea correcta
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
